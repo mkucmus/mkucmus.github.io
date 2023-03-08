a6c4a992ecd44600b82c9f3196235962675fd610
Maciek Kucmus <maciejkucmus@gmail.com>
2023-03-08 10:39:58 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,8 @@ export default defineNuxtConfig({
      */
     useMeta: "~/composables/useMeta",
   },
+  ssr: false,
+  target: "static",
   typescript: {
     typeCheck: true,
     strict: true,
